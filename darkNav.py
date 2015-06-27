0968e8d86024ba06efca116eb9153b093fe21ccf
Graham Hukill <ghukill@gmail.com>
2015-06-27 15:43:35 -0400

    command line options

--- darkNav.py
+++ darkNav.py
@@ -1,16 +1,18 @@
 import numpy
 import random
 import matplotlib.pyplot as plt
+import argparse
 
 
 class Navigator(object):
 
-	def __init__(self, name='ofNoName'):		
+	def __init__(self, name='Unknown', arena=(5000,5000)):				
 		self.coords = (0,0)
 		self.alive = True
 		self.name = name
 		self.history = []
 		self.history.append(self.coords)
+		self.arena = Arena(width=arena[0], height=arena[1])
 
 
 	def move(self, direction, distance):
@@ -47,10 +49,23 @@ class Navigator(object):
 
 		# create tuple
 		movement_tuple = tuple(numpy.multiply(dir_trans[direction],distance))
-		# print "movement_tuple",movement_tuple
 
-		# update navigator location
-		self.coords = tuple(numpy.add(self.coords, movement_tuple))
+		# try to move within arena
+		attempt_coords = numpy.add(self.coords, movement_tuple)
+
+		# check x
+		if attempt_coords[0] > self.arena.width:
+			attempt_coords[0] = self.arena.width
+		if attempt_coords[0] < -(self.arena.width):
+			attempt_coords[0] = -(self.arena.width)
+
+		# check y
+		if attempt_coords[1] > self.arena.height:
+			attempt_coords[1] = self.arena.height
+		if attempt_coords[1] < -(self.arena.height):
+			attempt_coords[1] = -(self.arena.height)
+
+		self.coords = tuple(attempt_coords)
 
 		# update history
 		self.history.append(self.coords)
@@ -78,28 +93,45 @@ class Navigator(object):
 		self.move(direction,distance)
 
 
+
 class Arena(object):
 
-	def __init__(self):
-		
-		self.height = 10
-		self.width = 10
+	def __init__(self,height,width):
+		self.height = height
+		self.width = width
 
 
 
 
 if __name__ == "__main__":
 	print "-----Entering darkNav-----"
+	print "reticulating splines..."
 
-	nav1 = Navigator('girgio')
+	# parse arguments
+	parser = argparse.ArgumentParser(description='Variables for darkNav')
+
+	parser.add_argument("--numNavs", type=int, help="Number of navigators to generate",default=4)
+	parser.add_argument("--arenah", type=int, help="Arena Height (radiating from 0,0)",default=500)
+	parser.add_argument("--arenaw", type=int, help="Arena Width (radiating from 0,0)",default=500)
+	parser.add_argument("--iterations", type=int, help="Number of times to move",default=5000)
+	parser.add_argument("--maxMove", type=int, help="Maximum distance navigator can move randomly",default=10)
+
+	args = parser.parse_args()
+
+	# create navigators
+	navigators = [Navigator(arena=(args.arenaw,args.arenah)) for i in range(args.numNavs)]
 
 	# random movements
-	for _ in range(10000):
-		nav1.randomMove(10)
+	for _ in range(args.iterations):
+		for nav in navigators:
+			nav.randomMove(args.maxMove)
+	
 
 	# prep to scatter
+	print "graphing results..."
 	from math import log
-	plt.scatter(*zip(*nav1.history))
+	for nav in navigators:
+		plt.plot(*zip(*nav.history))
 	plt.show()
 
 
