5e4fbe7530a5befbfd14aa0805c5be3312b5efc3
Graham Hukill <ghukill@gmail.com>
2015-07-03 00:34:06 -0400

    added iterative and navigator based threading options

--- darkNav.py
+++ darkNav.py
@@ -2,6 +2,8 @@ import numpy
 import random
 import matplotlib.pyplot as plt
 import argparse
+import threading
+import timeit
 
 
 class Navigator(object):
@@ -71,26 +73,28 @@ class Navigator(object):
 		self.history.append(self.coords)
 
 	# random move
-	def randomMove(self,max_distance):
+	def randomMove(self,max_distance,iterations):
 
-		# random direction
-		dir_dict = {
-			1:'n',
-			2:'e',
-			3:'s',
-			4:'w'
-		}
-		direction = dir_dict[int(random.random() * 4) + 1]
+		for i in range(iterations):
 
-		# random distance
-		flip = {
-			False:-1,
-			True:1
-		}
-		distance = (flip[bool(random.getrandbits(1))]) * (int(random.random() * max_distance) + 1)
+			# random direction
+			dir_dict = {
+				1:'n',
+				2:'e',
+				3:'s',
+				4:'w'
+			}
+			direction = dir_dict[int(random.random() * 4) + 1]
 
-		# debug
-		self.move(direction,distance)
+			# random distance
+			flip = {
+				False:-1,
+				True:1
+			}
+			distance = (flip[bool(random.getrandbits(1))]) * (int(random.random() * max_distance) + 1)
+
+			# debug
+			self.move(direction,distance)
 
 
 
@@ -99,6 +103,41 @@ class Arena(object):
 	def __init__(self,height,width):
 		self.height = height
 		self.width = width
+
+
+class navThread (threading.Thread):
+	def __init__(self, threadID, name, navigator,threadingType,iterations):
+		threading.Thread.__init__(self)
+		self.threadID = threadID
+		self.name = name
+		self.navigator = navigator
+		self.threadingType = threadingType
+		self.iterations = iterations
+
+	# threaded per navigation
+	def run(self):
+
+		'''
+		There's a tension here.
+		It's hard to squeeze the different architectures types
+		  into a single flag.
+		'''
+		
+		# interation_threaded
+		if self.threadingType in ["iteration_t","navigator_t"]:
+			self.navigator.randomMove(args.maxMove,self.iterations)
+
+		# # navigator_threaded
+		# if self.threadingType == "navigator_t":
+		# 	for i in range(args.iterations):
+		# 		self.navigator.randomMove(args.maxMove)
+
+		return True
+
+
+	
+
+
 
 
 
@@ -115,23 +154,53 @@ if __name__ == "__main__":
 	parser.add_argument("--arenaw", type=int, help="Arena Width (radiating from 0,0)",default=500)
 	parser.add_argument("--iterations", type=int, help="Number of times to move",default=5000)
 	parser.add_argument("--maxMove", type=int, help="Maximum distance navigator can move randomly",default=10)
+	parser.add_argument("--threadingType", type=str, help="Threading pattern (single_t,iteration_t,navigator_t)",default="single_t")
+	parser.add_argument("--makeGraph", type=str, help="Make graph (true, false)",default=True)
 
 	args = parser.parse_args()
 
 	# create navigators
-	navigators = [Navigator(arena=(args.arenaw,args.arenah)) for i in range(args.numNavs)]
+	navigators = [Navigator(arena=(args.arenaw,args.arenah),name=i) for i in range(args.numNavs)]
 
-	# random movements
-	for _ in range(args.iterations):
-		for nav in navigators:
-			nav.randomMove(args.maxMove)
-	
 
-	# prep to scatter
-	print "graphing results..."
-	from math import log
-	for nav in navigators:
-		plt.plot(*zip(*nav.history))
-	plt.show()
+	# fire based on threading type 
+	if args.threadingType == "single_t":
+		# single threaded random movements
+		def go():
+			for i in range(args.iterations):
+				for navigator in navigators:
+					navigator.randomMove(args.maxMove,iterations=1)
+
+
+	if args.threadingType == "iteration_t":					
+		# threaded for each iteration
+		def go():
+			for i in range(args.iterations):
+				for navigator in navigators:
+					navigator_thread = navThread(i,str(i),navigator,args.threadingType,iterations=1)
+					navigator_thread.start()
+
+
+	if args.threadingType == "navigator_t":
+		# threaded for each navigator
+		def go():
+			for navigator in navigators:
+				# isntantiate the navigator on a thread
+				navigator_thread = navThread(i,str(i),navigator,args.threadingType,iterations=args.iterations)
+				navigator_thread.start()
+			
+
+	print "Firing type:",args.threadingType			
+	print "Time to calculate:",timeit.timeit(go,number=1)
+
+
+	if bool(args.makeGraph) == True:
+		# prep to scatter
+		print "graphing results..."
+		from math import log
+		for navigator in navigators:
+			plt.plot(*zip(*navigator.history))
+		plt.show()
+		print "finis."
 
 
